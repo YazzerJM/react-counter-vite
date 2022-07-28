7e265fdb1e7f0ab16f58e4275789402a59d006c4
Yasser Jiménez Martínez <yasserjimenezmartinez@hotmail.com>
2022-07-27 19:15:44 -0500

    Primera fase realizada del counter

---
+++ src/FirstApp.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) => {
+
+    
+
+    return ( 
+        <>
+            <h1>{ title }</h1>
+            {/* <h1> { JSON.stringify(newMessage) } </h1>  */}
+            <p>{ subTitle }</p>
+            <p>{ name }</p>
+        </>
+    );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    name: 'Yasser Jimenez',
+    subTitle: 'No hay subtitulo',
+    title: 'No hay titulo',
+}
